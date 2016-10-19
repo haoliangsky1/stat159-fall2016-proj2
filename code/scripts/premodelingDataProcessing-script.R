b696d59d2397087d2798ea62cc22bdab9acf1c02
Liang Hao <haoliang302@gmail.com>
2016-10-19 16:55:55 -0700

    covert factors into dummy variables and mean centering and standardization

--- code/scripts/premodelingDataProcessing-script.R
+++ code/scripts/premodelingDataProcessing-script.R
@@ -4,3 +4,13 @@
 args = commandArgs(trailingOnly =TRUE)
 credit = read.csv(args[1], header = T)
 credit = credit[,2:ncol(credit)]
+
+# Covert factors into dummy variables
+temp_credit = model.matrix(Balance ~ ., data = credit)
+new_credit <- cbind(temp_credit[ ,-1], Balance = credit$Balance)
+
+# Mean centering and standardization
+scaled_credit = scale(new_credit, center = TRUE, scale = TRUE)
+write.csv(scaled_credit, file = "data/scaled-credit.csv")
+
+
