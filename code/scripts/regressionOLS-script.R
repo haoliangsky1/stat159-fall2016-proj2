e322f424fe59b9c7418f87715b2e019d11a7107c
Liang Hao <haoliang302@gmail.com>
2016-10-22 20:16:49 -0700

    initial commit

--- code/scripts/regressionOLS-script.R
+++ code/scripts/regressionOLS-script.R
@@ -1,7 +1,12 @@
+#setwd("~/Desktop/Fall_2016/Stat159/stat159-fall2016-proj2")
+#trainingSet = read.csv('data/.csv', header=T)
+
+
 # Fit OLS model:
 args = commandArgs(trailingOnly =TRUE)
 load(args[1])
 load(args[2])
-olsFit = lm()
+olsFit = lm(Balance~., data = trainingSet)
+save(olsFit, file = 'data/ols-regression-model')
 
 
