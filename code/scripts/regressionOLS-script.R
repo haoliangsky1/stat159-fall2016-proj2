b4345de1d4128230ff4c2b5c652f494a2ebc1486
Liang Hao <haoliang302@gmail.com>
2016-10-24 16:00:39 -0700

    update input method

--- code/scripts/regressionOLS-script.R
+++ code/scripts/regressionOLS-script.R
@@ -1,12 +1,13 @@
 #setwd("~/Desktop/Fall_2016/Stat159/stat159-fall2016-proj2")
-#trainingSet = read.csv('data/.csv', header=T)
-
-
+#trainingSet = read.csv('data/trainingSet.csv', header=T)
+#testSet = read.csv('data/testSet.csv', header = T)
+#scaledCredit = read.csv('data/scaled-credit.csv')
 # Fit OLS model:
 args = commandArgs(trailingOnly =TRUE)
-load(args[1])
+temp = read.csv(args[1], header = T)
+scaledCredit = temp[,2:ncol(temp)]
 load(args[2])
-olsFit = lm(Balance~., data = trainingSet)
+olsFit = lm(Balance~., data = scaledCredit, subset = trainingIndex)
 save(olsFit, file = 'data/ols-regression-model')
 
 
